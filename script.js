7a0db0dd0289573303b1aa61e57864c288116775
Sadli | Learn Data Science & AI/ML <176941072+curioussadli@users.noreply.github.com>
2025-11-28 15:06:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -287,7 +287,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // -------------------------
     // Judul laporan otomatis
     // -------------------------
-    let laporan = `📊 STOCK PERSEDIAAN\${tanggalLengkap}\n\n`;
+    let laporan = `📊 STOCK PERSEDIAAN\n${tanggalLengkap}\n\n`;
       
       let ada = false;
 
@@ -341,7 +341,7 @@ if (copyDashboardBtn) {
     // -------------------------
     // Judul laporan otomatis
     // -------------------------
-    let laporan = `📊 LAPORAN KAS\${tanggalLengkap}\n\n`;
+    let laporan = `📊 LAPORAN KAS\n${tanggalLengkap}\n\n`;
 
     const map = [
       ["Saldo Awal", "saldoAwal"],
@@ -521,6 +521,7 @@ function updateDashboard() {
     if (el) el.textContent = "Rp " + val.toLocaleString();
   }
 }
+
 
 
 
